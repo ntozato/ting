9249d50c3c05b5e05f84e520501b380b97e3d20c
Nícolas Tozato <ntozato9@gmail.com>
2022-02-06 19:23:56 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
+
+# referências
+# https://www.hashtagtreinamentos.com/trabalhar-com-arquivos-de-texto-python
+# https://codare.aurelio.net/2007/01/30/python-imprimir-mensagens-de-erro-stderr/
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write("Formato inválido\n")
+    
+    try:
+        with open(path_file, "r", encoding="utf-8") as file:
+            text = file.readlines()
+            
+            data = [row.strip('\n') for row in text]
+            return data
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
