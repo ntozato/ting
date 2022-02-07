2ffcb94af6889e39af1f006cc1b3a9f238c127e9
Nícolas Tozato <ntozato9@gmail.com>
2022-02-07 20:21:02 -0300

    requisito 7 - ultimo commit

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    pass
+    line = 0
+    ocurrencies = []
+    result = []
+
+    for item in instance.data:
+        for row in item["linhas_do_arquivo"]:
+            line += 1
+            if word.lower() in row.lower():
+                ocurrencies.append(
+                    {
+                        "linha": line,
+                        "conteudo": row
+                    }
+                )
+                result.append(
+                    {
+                        "palavra": word,
+                        "arquivo": item["nome_do_arquivo"],
+                        "ocorrencias": ocurrencies
+                    }
+                )
+
+    return result
