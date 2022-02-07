5e0fdfccb8972b8933362f71960facc3bdc504aa
Nícolas Tozato <ntozato9@gmail.com>
2022-02-07 20:13:21 -0300

    requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    line = 0
+    ocurrencies = []
+    result = []
+
+    for item in instance.data:
+        for row in item["linhas_do_arquivo"]:
+            line += 1
+            if word.lower() in row.lower():
+                ocurrencies.append({"linha": line})
+                result.append(
+                    {
+                        "palavra": word,
+                        "arquivo": item["nome_do_arquivo"],
+                        "ocorrencias": ocurrencies
+                    }
+                )
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    pass
